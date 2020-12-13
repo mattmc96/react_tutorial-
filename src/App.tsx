6235fec4cf62220a749d6f034e63e9b57b7e0154
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-13 01:24:17 -0600

    feat-app-header

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import AppHeader, { AppHeaderProps } from './containers/AppHeader';
+
+// Build an instance of our AppHeaderProps to pass to the AppHeader component
+const headerProps: AppHeaderProps = {
+  title: "My Dive Log",
+  description: "Log, Track, Review your dive logs and relive the experience",
+  links: [
+    {
+      label: "Logs",
+      route: "/logs/list"
+    },
+    {
+      label: "New Entry",
+      route: "/logs/create"
+    }
+  ]
+};
+
 const App: React.FC = () => {
   return (
     <main className="App">
---
+++ src/containers/AppHeader.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import "./AppHeader.css"
+import AppHeaderNavLink, { NavLink } from './AppHeaderNavLink'
+
+// Define our AppHeader properties that will be passed into the component
+export type AppHeaderProps = {
+    title: string;
+    description: string;
+    links: NavLink[];
+}
+
+// Create React Functional Component variable that will render our code for our header
+// React.FC takes a type of the props that can be passed into the component
+const AppHeader: React.Fc<AppHeaderProps> = React.memo(({ title, description, links }) => {
+        return (
+            <header className="app-header">
+                <section className="app-header">
+                    <h1>{title}</h1>
+                    <small>{description}</small>
+                </section>
+                <span className="fill-space">
+                    <section className="app-links">
+                        {links &&
+                        links.map(( link: NavLink) => (
+                            <AppHeaderNavLink
+                            label={link.label}
+                            route={link.route}
+                            key={link.label}
+                            />
+                        ))}
+                    </section>
+                </span>
+            </header>
+        )
+   }
+)
+export default AppHeader
