5fcad73fff7a2f64910cde6e2fda90c7078493a9
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-13 12:28:07 -0600

    feat: Added new components

--- src/containers/AppBody.tsx
+++ src/containers/AppBody.tsx
@@ -1,1 +1,16 @@
 import React from 'react'
+import { TextField } from './TextField"'
+
+// Props
+// Hooks
+// Render Props
+
+const App: React.FC = () => {
+    return (
+        <TextField text='hello'/>
+        <div>
+    </div>
+)
+}
+
+export default App
